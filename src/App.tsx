4ad4b7f7faf33e06e419fb6db5b0cb8c6f1c7e39
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-08-30 14:44:48 +0900

    Feat: 스케쥴 페이지 calendar 컴포넌트 작성 추가적으로 공휴일, 토요일에 맞도록 글씨 색 변경 반영 예정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import MainPage from '../src/pages/MainPage';
+import SchedulePage from './pages/SchedulePage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path='/schedule' element={<SchedulePage />} />
         </Routes>
       </BrowserRouter>
     </div>
