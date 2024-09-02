73af18b3129386667e594e232e16adb1d1d925d1
boeun <supbe0726@naver.com>
2024-09-02 18:58:17 +0900

    Feat: 메인, 스토리 페이지 작업

---
+++ src/components/main/Story.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  padding: 30px 0 30px;
+`;
+const Title = styled.h2`
+  font-size: 20px;
+  padding: 0 15px 12px;
+`;
+const SlideCont = styled.div`
+  padding-left: 15px;
+`;
+const SlideImg = styled.div`
+  & img {
+    width: 100%;
+  }
+  height: 45vw;
+  background: #d9d9d9;
+  border-radius: 15px;
+`;
+const StoryTitle = styled.p`
+  padding-top: 5px;
+  font-size: 14px;
+  font-weight: 600;
+`;
+const Story = () => {
+  return (
+    <Container>
+      <Title>JOHA Story</Title>
+      <SlideCont>
+        <Swiper spaceBetween={15} slidesPerView={1.3}>
+          <SwiperSlide>
+            <Link to="">
+              <SlideImg>
+                <img />
+              </SlideImg>
+              <StoryTitle>스토리 제목</StoryTitle>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="">
+              <SlideImg>
+                <img />
+              </SlideImg>
+              <StoryTitle>스토리 제목</StoryTitle>
+            </Link>
+          </SwiperSlide>
+        </Swiper>
+      </SlideCont>
+    </Container>
+  );
+};
+
+export default Story;
