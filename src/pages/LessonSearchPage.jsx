78551a5f15de42c82795b00df1baccd53679194e
boeun <supbe0726@naver.com>
2024-09-07 01:39:36 +0900

    DesignCSS: css 수정

---
+++ src/pages/LessonSearchPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/layout/Header";
+import LessonSearch from "../components/search/LessonSearch";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header />
+      <LessonSearch />
+    </>
+  );
+};
+
+export default LoginPage;
