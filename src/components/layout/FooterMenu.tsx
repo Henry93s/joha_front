6a7a743246670427262cdea1661886b5ca37b713
boeun <supbe0726@naver.com>
2024-09-07 01:31:34 +0900

    Feat: 검색 페이지 연결

--- src/components/layout/FooterMenu.tsx
+++ src/components/layout/FooterMenu.tsx
@@ -7,56 +7,55 @@ import { ReactComponent as Search } from "../../assets/icons/search.svg"; // SVG
 import { ReactComponent as Story } from "../../assets/icons/story.svg"; // SVG 파일을 컴포넌트로 import
 
 const FooterDiv = styled.div`
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    max-width: 700px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 15px;
-    height: 67px;
-    border-top: 1px solid #e6e6e6;
-    background: #fff;
-    z-index: 10;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  max-width: 700px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 67px;
+  border-top: 1px solid #e6e6e6;
+  background: #fff;
+  z-index: 10;
 `;
 
 const MenuItem = styled(Link)`
-    text-align: center;
-    color: inherit;
+  text-align: center;
+  color: inherit;
 
-    &.active {
-        color: var(--main-color);
-    }
-    &.active svg path {
-        fill: var(--main-color);
-    }
+  &.active {
+    color: var(--main-color);
+  }
+  &.active svg path {
+    fill: var(--main-color);
+  }
 `;
 
 const MenuLabel = styled.div`
-    font-size: 12px;
-    padding-top: 3px;
+  font-size: 12px;
+  padding-top: 3px;
 `;
 
 const items = [
-    { component: Search, name: "검색", src: "" },
-    { component: Heart, name: "찜 목록", src: "" },
-    { component: Schedule, name: "스케줄", src: "" },
-    { component: MyClass, name: "나의 수업", src: "" },
-    { component: Story, name: "스토리", src: "" },
+  { component: Search, name: "검색", src: "/search" },
+  { component: Heart, name: "찜 목록", src: "" },
+  { component: Schedule, name: "스케줄", src: "" },
+  { component: MyClass, name: "나의 수업", src: "" },
+  { component: Story, name: "스토리", src: "" },
 ];
 
 const FooterMenu = () => {
-    return (
-        <FooterDiv>
-            {items.map(({ component: Component, name, src }) => (
-                <MenuItem key={name} to={src}>
-                    <Component />
-                    <MenuLabel>{name}</MenuLabel>
-                </MenuItem>
-            ))}
-        </FooterDiv>
-    );
+  return (
+    <FooterDiv>
+      {items.map(({ component: Component, name, src }) => (
+        <MenuItem key={name} to={src}>
+          <Component />
+          <MenuLabel>{name}</MenuLabel>
+        </MenuItem>
+      ))}
+    </FooterDiv>
+  );
 };
 
 export default FooterMenu;
