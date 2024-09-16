c1b779e1e7bc3492d7a3d7d2678dae96fbffac5c
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-09-16 10:21:46 +0900

    Fix: import 시 jsx 명시 수정(추후 tsx 로 마이그레이션 예정)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/layout/Header";
-import Login from "../components/account/Login";
+import Login from "../components/account/Login.jsx";
 import SubLayout from "../components/layout/SubLayout";
 
 const LoginPage = () => {
