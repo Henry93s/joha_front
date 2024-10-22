d62517ee9d659a56bf241d70e3d0e68bbb28af13
boeun <supbe0726@naver.com>
2024-10-22 22:34:43 +0900

    Fix: 배열값이 리턴되게 수정

--- src/api/useKakaoLoader.js
+++ src/api/useKakaoLoader.js
@@ -4,7 +4,6 @@ const useKakaoLoaderOrigin = () => {
     appkey: process.env.REACT_APP_KAKAO_APP_KEY,
     libraries: ["clusterer", "services"], // 사용하고싶은 다른 라이브러리 추가
   });
-
   return [loading, error];
 };
 
