d53ffc2c59ea268a879db85a895aee18c7407a06
boeun <supbe0726@naver.com>
2024-10-24 14:20:04 +0900

    Fix: 요청주소 오타 수정

--- src/api/myPostUpload.js
+++ src/api/myPostUpload.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 export const myPostUpload = async (formData) => {
   try {
-    const res = await axios.post("http://localhost/3002/class", formData, {
+    const res = await axios.post("http://localhost:3002/class", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
+      withCredentials: true,
       // 쿠키를 포함시키기 위해 필요
     });
     return res;
